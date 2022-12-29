1847853ca4e897f889c5303e1ed66a4d82870f90
UK <gang3351@gmail.com>
2022-12-29 15:02:11 +0900

    Initial Commit

---
+++ Hello Swift!/Hello_Swift_App.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_Swift_App.swift
+//  Hello Swift!
+//
+//  Created by 임강욱 on 2022/12/29.
+//
+
+import SwiftUI
+
+@main
+struct Hello_Swift_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
