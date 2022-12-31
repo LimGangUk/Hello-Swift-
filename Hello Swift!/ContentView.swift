07d49231e029c3afb7e29f8e8aa8a00447a9a996
UK <gang3351@gmail.com>
2022-12-31 16:30:58 +0900

    12/31 이미지 파일 속성

--- Hello Swift!/ContentView.swift
+++ Hello Swift!/ContentView.swift
@@ -10,16 +10,25 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.orange)
+            Image("instagram_logo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100.0, height: 100.0)
+                
+                .clipShape(Circle())// 둥근 테두리
+                .overlay{
+                    Circle().stroke(.gray, lineWidth: 4)            //테두리에 선 긋기
+                }
+                .shadow(radius: 6)//테두리 그림자
+            
             Text("Hello, Swift!")
-                .foregroundColor(Color.orange)
+                .foregroundColor(Color(hue: 0.84, saturation: 0.663, brightness: 0.996))
                 .font(.system(size : 40, weight: .black, design: .serif))
             HStack {
                 Text("subText1")
-                    .foregroundColor(Color.yellow)
-                .font(.system(size : 20, weight: .black, design: .serif))
+                    .font(.system(size : 20, weight: .black, design: .serif))
+                    .foregroundColor(Color(hue: 0.606, saturation: 0.595, brightness: 0.918))
+                    
                 Spacer()
                     .frame(width: 40)
                 Text("subText2")
